56467b57f45a1ae27da6114ba4569cde88431bd4
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2018-01-17 16:33:26 +0600

    porject created

---
+++ src/main/java/com/coderslab/BootSampleProjectApplication.java
@@ -0,0 +1,12 @@
+package com.coderslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSampleProjectApplication.class, args);
+	}
+}
