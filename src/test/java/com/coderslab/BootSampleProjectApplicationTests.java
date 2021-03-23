c67f083e3a19713231308cc465ba646efa88906b
zubayerahamed <zubayerahamed.freelancer@gmail.com>
2021-03-23 10:22:52 +0600

    c

--- src/test/java/com/coderslab/BootSampleProjectApplicationTests.java
+++ src/test/java/com/coderslab/BootSampleProjectApplicationTests.java
@@ -1,16 +1,34 @@
 package com.coderslab;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.Test;
+
+
 public class BootSampleProjectApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws ParseException {
+
+		String dateString = "2021-04";
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+		Date date = sdf.parse(dateString);
+		Calendar currentMonth = Calendar.getInstance();
+		Calendar nextMonth = Calendar.getInstance();
+		nextMonth.add(Calendar.MONTH, 1);
+		Calendar actualMonth = Calendar.getInstance();
+		actualMonth.setTime(date);
+
+		if(currentMonth.get(Calendar.YEAR) == actualMonth.get(Calendar.YEAR)) {
+			if((currentMonth.get(Calendar.MONTH) == actualMonth.get(Calendar.MONTH)) || (nextMonth.get(Calendar.MONTH) == actualMonth.get(Calendar.MONTH))) {
+				System.out.println(true);
+			}
+		}
+
 	}
 
 }
